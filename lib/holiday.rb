40d328583bbbf74cf0e8b3ef87abec4611ddade5
Umar Khan <umarkhandev@gmail.com>
2019-03-07 17:43:34 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -57,6 +57,7 @@ def all_supplies_in_holidays(holiday_hash)
   # etc.
   holiday_hash.each do |season, holiday|
     holiday.each do |name, dec|
+      season = season.join(" ")
       puts "#{season}:".capitalize
       name = name.capitalize
       dec = dec.join(", ")
